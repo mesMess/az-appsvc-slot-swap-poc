d138a82cbba886ae471edb9b3f424e6d54cacf65
Michael Smithzer <mesMess@users.noreply.github.com>
2022-02-06 13:59:52 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Azure.Management.ResourceManager.Fluent.Core;
+using Microsoft.Azure.Management.AppService.Fluent;
+
+using IHost host = Host.CreateDefaultBuilder(args)
+.ConfigureAppConfiguration((hostingContext, configuration) =>
+{
+    configuration.Sources.Clear();
+
+    IHostEnvironment env = hostingContext.HostingEnvironment;
+
+    configuration
+        .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+        .AddJsonFile($"appsettings.{env.EnvironmentName}.json", true, true);
+
+    IConfigurationRoot configurationRoot = configuration.Build();
+
+    TransientFaultHandlingOptions options = new();
+    configurationRoot.GetSection(nameof(TransientFaultHandlingOptions))
+                     .Bind(options);
+
+    Console.WriteLine($"TransientFaultHandlingOptions.Enabled={options.Enabled}");
+    Console.WriteLine($"TransientFaultHandlingOptions.AutoRetryDelay={options.AutoRetryDelay}");
+})
+.Build();
+
+Console.WriteLine("Hello, World!");
+
+// IWebApp app1 =
+
+await host.RunAsync();
+
+public class TransientFaultHandlingOptions
+{
+    public bool Enabled { get; set; }
+    public TimeSpan AutoRetryDelay { get; set; }
+}
---
+++ README.md
@@ -0,0 +1,12 @@
+# Azure AppServices Deployment Slot - P.O.C
+
+## Description
+This is a light weight service to test the ease of use for working with
+Azure AppServices Deployment Slots
+
+## Tech
+
+- Framework: .Net 6
+- Language: C#
+- IDE: VS Code
+- Additional: Nuget package - Fluent
